403774e5c017c116db9508bc97ec7ca05b85919a
hseun <hseun0732@dms.hs.kr>
2025-04-15 17:58:00 +0900

    style :: singleQuote

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -1,14 +1,14 @@
 export const color = {
-  transparent: "",
-  white: "#FFFFFF",
-  black: "#000000",
-  gray100: "#F1F1F1",
-  gray200: "#EDEDED",
-  gray300: "#DFDFDF",
-  gray400: "#B3B3B3",
-  gray500: "#989898",
-  gray600: "#5E5E5E",
-  pink100: "#FFF0FC",
-  pink200: "#F85AB6",
-  pink300: "#F03E8F"
+  transparent: '',
+  white: '#FFFFFF',
+  black: '#000000',
+  gray100: '#F1F1F1',
+  gray200: '#EDEDED',
+  gray300: '#DFDFDF',
+  gray400: '#B3B3B3',
+  gray500: '#989898',
+  gray600: '#5E5E5E',
+  pink100: '#FFF0FC',
+  pink200: '#F85AB6',
+  pink300: '#F03E8F'
 };
--- src/styles/font.tsx
+++ src/styles/font.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Text, TextStyle } from "react-native";
-import { color } from "./color";
+import React from 'react';
+import { Text, TextStyle } from 'react-native';
+import { color } from './color';
 
 interface FontPropsType {
   text?: string;
@@ -8,14 +8,14 @@ interface FontPropsType {
   color?: keyof typeof color;
   style?: TextStyle;
   numberOfLines?: number;
-  ellipsizeMode?: "clip" | "head" | "middle" | "tail";
+  ellipsizeMode?: 'clip' | 'head' | 'middle' | 'tail';
 }
 
 export const Font = ({
   ellipsizeMode,
   text,
-  kind = "",
-  color: textColor = "black",
+  kind = '',
+  color: textColor = 'black',
   numberOfLines,
   style
 }: FontPropsType) => {
@@ -31,85 +31,85 @@ export const Font = ({
 };
 
 const fonts: { [key: string]: TextStyle } = {
-  "": {},
+  '': {},
   medium14: {
     fontSize: 14,
     lineHeight: 18,
-    fontWeight: "500"
+    fontWeight: '500'
   },
   semi14: {
     fontSize: 14,
     lineHeight: 18,
-    fontWeight: "600"
+    fontWeight: '600'
   },
   bold14: {
     fontSize: 14,
     lineHeight: 18,
-    fontWeight: "700"
+    fontWeight: '700'
   },
   medium16: {
     fontSize: 16,
     lineHeight: 20,
-    fontWeight: "500"
+    fontWeight: '500'
   },
   semi16: {
     fontSize: 14,
     lineHeight: 20,
-    fontWeight: "600"
+    fontWeight: '600'
   },
   bold16: {
     fontSize: 16,
     lineHeight: 20,
-    fontWeight: "700"
+    fontWeight: '700'
   },
   medium18: {
     fontSize: 18,
     lineHeight: 22,
-    fontWeight: "500"
+    fontWeight: '500'
   },
   semi18: {
     fontSize: 18,
     lineHeight: 22,
-    fontWeight: "600"
+    fontWeight: '600'
   },
   bold18: {
     fontSize: 18,
     lineHeight: 22,
-    fontWeight: "700"
+    fontWeight: '700'
   },
   medium20: {
     fontSize: 20,
     lineHeight: 24,
-    fontWeight: "500"
+    fontWeight: '500'
   },
   semi20: {
     fontSize: 20,
     lineHeight: 24,
-    fontWeight: "600"
+    fontWeight: '600'
   },
   medium24: {
     fontSize: 24,
     lineHeight: 36,
-    fontWeight: "500"
+    fontWeight: '500'
   },
   semi24: {
     fontSize: 24,
     lineHeight: 36,
-    fontWeight: "600"
+    fontWeight: '600'
   },
   bold24: {
     fontSize: 24,
     lineHeight: 36,
-    fontWeight: "700"
+    fontWeight: '700'
   },
   semi28: {
     fontSize: 28,
     lineHeight: 46,
-    fontWeight: "600"
+    fontWeight: '600'
   },
   black28: {
     fontSize: 28,
     lineHeight: 46,
-    fontWeight: "900"
+    fontWeight: '900'
   }
 };
