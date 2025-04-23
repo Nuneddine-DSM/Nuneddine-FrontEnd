d9be2cb0018118c098b2cf5c28582f81aa0c6b69
limda00 <dylim7715@gmail.com>
2025-04-23 14:53:36 +0900

    style :: 상품 카드 컴포넌트 다크모드 적용

---
+++ src/components/Shopping/ProductCardLarge.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { Heart } from "../../assets"
+import { Font, color } from "../../styles"
+import styled, { ThemeProvider, DefaultTheme } from "styled-components/native";
+import { ShoppingContentType } from "../../app/Main/interface";
+import Tag from "./Tag"
+
+/**
+ * 상품 카드 큰 버전
+ */
+
+export const lightTheme: DefaultTheme = {
+  background: "white",
+  text: "black",
+};
+
+export const darkTheme: DefaultTheme = {
+  background: "black",
+  text: "white",
+};
+
+interface CardPropsType extends ShoppingContentType {
+  isDarkMode?: boolean;
+}
+
+const ProductCardLarge = ({
+  isDarkMode = false,
+  image,
+  title,
+  describe,
+  tag,
+  price
+}: CardPropsType) => {
+  const theme = isDarkMode ? darkTheme : lightTheme;
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CardContainer background={theme.background}>
+        <ImageWrapper>
+          <ProductImage source={{ uri: image }} />
+          <IconWrapper>
+            <Heart
+              size={30}
+              color={color.gray500}
+              fill={selected ? color.pink300 : "none"}
+              onPress={() => setSelected(prev => !prev)}
+            />
+          </IconWrapper>
+        </ImageWrapper>
+
+        <InfoWrapper>
+          <TitleBox>
+            <Font
+              text={title}
+              kind="bold20"
+              color={theme.text}
+            />
+            <Font
+              text={describe}
+              kind="regular18"
+              color={theme.text}
+              numberOfLines={2}
+              ellipsizeMode="tail"
+            />
+          </TitleBox>
+
+          <Tag text={tag} isDark={true} />
+
+          <Font
+            text={`${price.toLocaleString()}원`}
+            kind="bold18"
+            color={theme.text}
+          />
+        </InfoWrapper>
+      </CardContainer>
+    </ThemeProvider>
+  );
+};
+
+const CardContainer = styled.View<{ background: string }>`
+  width: 187.5px;
+  flex-direction: column;
+  gap: 12px;
+  background-color: ${({ background }) => background};
+`
+
+const ImageWrapper = styled.View`
+  position: relative;
+  width: 187.5px;
+  height: 187.5px;
+  background-color: ${color.gray200};
+`
+
+const ProductImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`
+
+const IconWrapper = styled.View`
+  position: absolute;
+  bottom: 12px;
+  right: 12px;
+`
+
+const InfoWrapper = styled.View`
+  flex-direction: column;
+  gap: 14px;
+`
+
+const TitleBox = styled.View`
+  flex-direction: column;
+  gap: 7px;
+`
+
+export default ProductCardLarge;
