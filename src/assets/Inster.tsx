58705fa55247abe0a5112eecb240e6c1f590f3e0
limda00 <dylim7715@gmail.com>
2025-04-23 12:05:03 +0900

    refactor :: Footer 리팩토링

--- src/assets/Inster.tsx
+++ src/assets/Inster.tsx
@@ -6,8 +6,6 @@ export const Inster = ({ size = 24, color = 'black', onPress }: SvgPropsType) =>
     <Svg width={size} height={size} fill="none" viewBox="0 0 16 16" onPress={onPress}>
       <Path stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M11 1H5a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h6a4 4 0 0 0 4-4V5a4 4 0 0 0-4-4Z" clipRule="evenodd" />
       <Path stroke={color} strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" clipRule="evenodd" />
-      <rect width="2" height="2" x="11" y="5" fill="#000" rx="1" transform="rotate(-90 11 5)" />
-      <rect width="1" height="1" x="11.5" y="4.5" stroke={color} strokeLinecap="round" rx=".5" transform="rotate(-90 11.5 4.5)" />
     </Svg>
   )
 }
--- src/components/Main/Footer.tsx
+++ src/components/Main/Footer.tsx
@@ -1,73 +1,68 @@
 import styled from "styled-components/native";
 import { FooterData } from "../../app/Main/Data";
+import { Inster, Message } from "../../assets";
+import { color, Font } from "../../styles"
 
 const Footer = () => {
-  return (
-    <FooterContainer>
-      <FooterInfoSection>
-        {FooterData.map(({ id, title, info }) => (
-          <FooterInfoItem key={id}>
-            <FooterLabel>{title}</FooterLabel>
-            <FooterText>{info}</FooterText>
-          </FooterInfoItem>
-        ))}
-      </FooterInfoSection>
+  const SNSIcons = [
+    { id: "inster", Icon: Inster },
+    { id: "message", Icon: Message }
+  ];
 
-      <FooterSNSSection>
-        <SNSIcon></SNSIcon>
-        <SNSIcon></SNSIcon>
-      </FooterSNSSection>
-    </FooterContainer>
+  return (
+    <Container>
+      <InfoSection>
+        {FooterData.map(({ id, title, info }) => (
+          <InfoRow key={id}>
+            <Font text={title} kind="medium14" color="gray500" />
+            <Font text={info} kind="semi14" color="gray600" />
+          </InfoRow>
+        ))}
+      </InfoSection>
+
+      <SNSSection>
+        {SNSIcons.map(({ id, Icon }) => (
+          <IconWrapper key={id}>
+            <Icon size={14} color={color.white} />
+          </IconWrapper>
+        ))}
+      </SNSSection>
+    </Container>
   );
 };
 
-const FooterContainer = styled.View`
-  display: flex;
+const Container = styled.View`
   flex-direction: column;
+  padding: 34px 20px 40px;
   gap: 24px;
-  padding: 34px 20px;
   border-top-width: 2px;
-  border-color: red;
+  border-color: ${color.gray200};
+  background-color: ${color.gray50};
 `
 
-const FooterInfoSection = styled.View`
-  display: flex;
+const InfoSection = styled.View`
   flex-direction: column;
   gap: 15px;
 `
 
-const FooterInfoItem = styled.View`
-  display: flex;
+const InfoRow = styled.View`
   flex-direction: row;
+  align-items: center;
   gap: 14px;
 `
 
-const FooterLabel = styled.Text`
-  font-size: 14px;
-  font-weight: 500;
-  color: gray;
-`
-
-const FooterText = styled.Text`
-  font-size: 14px;
-  font-weight: 600;
-  color: gray;
-`
-
-const FooterSNSSection = styled.View`
-  display: flex;
+const SNSSection = styled.View`
   flex-direction: row;
   gap: 9px;
-`
+`;
 
-const SNSIcon = styled.View`
+const IconWrapper = styled.View`
   width: 24px;
   height: 24px;
-  display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 100px;
-  background-color: red;
+  background-color: ${color.gray200};
 `
 
 export default Footer
