cb8a0db8809f7cda917e13454312a94d13a0df1e
limda00 <dylim7715@gmail.com>
2025-04-29 19:08:10 +0900

    add :: 폰트 및 색상 추가

--- src/styles/color.ts
+++ src/styles/color.ts
@@ -13,6 +13,8 @@ export const color = {
   pink100: '#FFF0FC',
   pink200: '#F85AB6',
   pink300: '#F03E8F',
-  yellow: '#FFD64E',
+  pink400: '#FF4196',
+  yellow100: '#FFD64E',
+  yellow200: '#FAE246',
   brown: '#201717'
 };
