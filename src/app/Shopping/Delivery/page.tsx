350a2906a750eb3dab8f6c8d95bd36433763553e
hseun <hseun0732@dms.hs.kr>
2025-05-08 14:33:58 +0900

    chore :: prettier

--- src/app/Shopping/Delivery/page.tsx
+++ src/app/Shopping/Delivery/page.tsx
@@ -1,35 +1,38 @@
-import styled from "styled-components/native";
-import { Font, color } from "../../../styles"
-import { TopBar } from "../../../components";
-import { TouchableOpacity } from "react-native";
-import { Arrow } from "../../../assets";
-import DeliveryList from "../../../components/Shopping/Delivery";
+import styled from 'styled-components/native';
+import { Font, color } from '../../../styles';
+import { TopBar } from '../../../components';
+import { TouchableOpacity } from 'react-native';
+import { Arrow } from '../../../assets';
+import DeliveryList from '../../../components/Shopping/Delivery';
 
 export interface DeliveryType {
-  selected?: boolean,
-  nickName?: string,
-  name?: string,
-  phone?: string,
-  address?: string,
-  check?: boolean
+  addressId?: number;
+  selected?: boolean;
+  nickName?: string;
+  name?: string;
+  phone?: string;
+  address?: string;
+  check?: boolean;
 }
 
 const DeliveryData: DeliveryType[] = [
   {
     selected: true,
-    nickName: "제 2의 집",
-    name: "박예빈",
-    phone: "010-1234-1234",
-    address: "대전광역시 유성구 가정북로 76 (장동, 대덕소프트웨어마이스터고등학교), 우정관 택배함(기숙사)"
+    nickName: '제 2의 집',
+    name: '박예빈',
+    phone: '010-1234-1234',
+    address:
+      '대전광역시 유성구 가정북로 76 (장동, 대덕소프트웨어마이스터고등학교), 우정관 택배함(기숙사)'
   },
   {
     selected: false,
-    nickName: "대마고",
-    name: "임다영",
-    phone: "010-1234-1234",
-    address: "대전광역시 유성구 가정북로 76 (장동, 대덕소프트웨어마이스터고등학교), 우정관 택배함(기숙사)"
-  },
-]
+    nickName: '대마고',
+    name: '임다영',
+    phone: '010-1234-1234',
+    address:
+      '대전광역시 유성구 가정북로 76 (장동, 대덕소프트웨어마이스터고등학교), 우정관 택배함(기숙사)'
+  }
+];
 
 const Delivery = () => {
   return (
@@ -37,7 +40,7 @@ const Delivery = () => {
       <TopBar
         text="배송지 목록"
         leftIcon={
-          <TouchableOpacity onPress={() => { }}>
+          <TouchableOpacity onPress={() => {}}>
             <Arrow size={34} />
           </TouchableOpacity>
         }
@@ -52,8 +55,8 @@ const Delivery = () => {
         </AddButton>
       </DeliveryListWrapper>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.View`
   flex: 1;
@@ -61,13 +64,13 @@ const Container = styled.View`
   gap: 15px;
   padding-top: 62px;
   background-color: ${color.white};
-`
+`;
 
 const DeliveryListWrapper = styled.View`
   flex-direction: column;
   padding: 35px 18px;
   gap: 16px;
-`
+`;
 
 const AddButton = styled.TouchableOpacity`
   display: flex;
@@ -79,6 +82,6 @@ const AddButton = styled.TouchableOpacity`
   border-radius: 5px;
   border-width: 1px;
   border-color: ${color.gray300};
-`
+`;
 
-export default Delivery
+export default Delivery;
