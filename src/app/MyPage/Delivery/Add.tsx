9457fc6b50ffd4e93f3be40d4a0a616ec0e37f6c
hseun <hseun0732@dms.hs.kr>
2025-05-08 16:18:06 +0900

    feature :: 주소 등록

--- src/app/MyPage/Delivery/Add.tsx
+++ src/app/MyPage/Delivery/Add.tsx
@@ -1,12 +1,42 @@
 import styled from 'styled-components/native';
 import { color, Font } from '../../../styles';
 import { Button, Input, TopBar } from '../../../components';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import { Arrow } from '../../../assets';
 import { useNavigation } from '@react-navigation/native';
+import { useState } from 'react';
+import { addAddress, AddAddressRequest } from '../../../apis/address';
 
 const Add = () => {
   const navigation = useNavigation();
+
+  const [postNumber, setPostNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [detailAddress, setDetailAddress] = useState('');
+  const [addressName, setAddressName] = useState('');
+  const [personName, setPersonName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const postAddress = async () => {
+    try {
+      const requestData: AddAddressRequest = {
+        postNumber: postNumber,
+        address: address,
+        detailAddress: detailAddress,
+        addressName: addressName,
+        personName: personName,
+        phone: phone
+      };
+      const response = await addAddress(requestData);
+      if (response.status == 200) {
+        navigation.goBack();
+      } else {
+        Alert.alert('배송지 등록 실패');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <AddAddressContainer>
@@ -22,31 +52,72 @@ const Add = () => {
         <FormSection>
           <Font text="주소" kind="semi16" color="gray600" />
           <RowInputWrapper>
-            <Input width="280px" placeholder="우편번호를 입력해주세요." />
+            <Input
+              width="280px"
+              placeholder="우편번호를 입력해주세요."
+              value={postNumber}
+              onChangeText={setPostNumber}
+            />
             <Button text="찾기" width="80px" />
           </RowInputWrapper>
-          <Input placeholder="주소지를 입력해주세요." />
-          <Input placeholder="상세주소를 입력해주세요." />
+          <Input
+            placeholder="주소지를 입력해주세요."
+            value={address}
+            onChangeText={setAddress}
+          />
+          <Input
+            placeholder="상세주소를 입력해주세요."
+            value={detailAddress}
+            onChangeText={setDetailAddress}
+          />
         </FormSection>
 
         <FormSection>
           <Font text="배송지명" kind="semi16" color="gray600" />
-          <Input placeholder="집, 회사 등 배송지명을 입력해주세요." />
+          <Input
+            placeholder="집, 회사 등 배송지명을 입력해주세요."
+            value={addressName}
+            onChangeText={setAddressName}
+          />
         </FormSection>
 
         <FormSection>
           <Font text="수령인" kind="semi16" color="gray600" />
-          <Input placeholder="이름을 입력해주세요." />
+          <Input
+            placeholder="이름을 입력해주세요."
+            value={personName}
+            onChangeText={setPersonName}
+          />
         </FormSection>
 
         <FormSection>
           <Font text="전화번호" kind="semi16" color="gray600" />
-          <Input placeholder="전화번호를 입력해주세요." />
+          <Input
+            placeholder="전화번호를 입력해주세요."
+            value={phone}
+            onChangeText={setPhone}
+          />
         </FormSection>
       </FormWrapper>
 
       <ButtonWrapper>
-        <Button text="저장하기" />
+        <Button
+          text="저장하기"
+          onPress={() => {
+            if (
+              !postNumber ||
+              !address ||
+              !detailAddress ||
+              !addressName ||
+              !personName ||
+              !phone
+            ) {
+              Alert.alert('모두 입력해주세요');
+            } else {
+              postAddress();
+            }
+          }}
+        />
       </ButtonWrapper>
     </AddAddressContainer>
   );
