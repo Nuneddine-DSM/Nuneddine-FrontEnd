aca0de1c9eed5ac4fcef37acbd36f02647e9ad7f
hseun <hseun0732@dms.hs.kr>
2025-06-03 22:10:08 +0900

    setting :: package

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
     [
       'module:react-native-dotenv',
       {
@@ -15,6 +14,7 @@ module.exports = {
         safe: false,
         allowUndefined: true
       }
-    ]
+    ],
+    'react-native-reanimated/plugin'
   ]
 };
