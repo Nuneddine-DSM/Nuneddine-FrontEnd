e21c50f5211189fe4364e89cdf8922455c865d74
hseun <hseun0732@dms.hs.kr>
2025-06-03 22:29:23 +0900

    add :: MyLensListItem

--- src/components/Manage/MyLensListItem.tsx
+++ src/components/Manage/MyLensListItem.tsx
@@ -4,15 +4,41 @@ import { Button } from '../Button';
 import { MyLensItemData } from '../../apis/alarms';
 import LinearGradient from 'react-native-linear-gradient';
 import { LensDateTypeMap } from '../../app/Data';
+import { useState } from 'react';
+import { Animated, Easing, View } from 'react-native';
 
 interface MyLensListItmeProps {
   item: MyLensItemData;
-  onPress?: () => void;
+  isExpended?: boolean;
+  onButtonPress?: () => void;
+  onExpendedPress?: () => void;
 }
 
-const MyLensListItem = ({ item, onPress }: MyLensListItmeProps) => {
+const MyLensListItem = ({
+  item,
+  isExpended,
+  onButtonPress,
+  onExpendedPress
+}: MyLensListItmeProps) => {
+  const [isOn, setIsOn] = useState(false);
+  const [aniValue, setAniValue] = useState(new Animated.Value(0));
+
+  const moveSwitchToggle = aniValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 26]
+  });
+
+  Animated.timing(aniValue, {
+    toValue: isOn ? 1 : 0,
+    duration: 200,
+    easing: Easing.linear,
+    useNativeDriver: true
+  }).start();
+
   return (
-    <Container>
+    <Container
+      disabled={!Boolean(item.start_time && item.end_time)}
+      onPress={onExpendedPress}>
       {item.end_time && (
         <DueDateWrapper>
           <Font
@@ -46,12 +72,54 @@ const MyLensListItem = ({ item, onPress }: MyLensListItmeProps) => {
             />
           </ProgressBarContainer>
           <UseLensDate>
-            <Font text={item.start_time} kind="regular16" color="gray300" />
-            <Font text={item.end_time} kind="regular16" color="gray300" />
+            <Font
+              text={dateFormat(item.start_time)}
+              kind="regular16"
+              color="gray300"
+            />
+            <Font
+              text={dateFormat(item.end_time)}
+              kind="regular16"
+              color="gray300"
+            />
           </UseLensDate>
         </UseLensPersentWrapper>
       ) : (
-        <Button text="START" buttonColor="black" onPress={onPress} />
+        <Button text="START" buttonColor="black" onPress={onButtonPress} />
+      )}
+      {isExpended && item.end_time && (
+        <View style={{ gap: 0 }}>
+          <DetailInfoWrapper>
+            <Font text="제품명" kind="semi16" />
+            <Font text={item.name} kind="medium16" color="gray500" />
+          </DetailInfoWrapper>
+          <DetailInfoWrapper>
+            <Font text="소비기한" kind="semi16" />
+            <Font
+              text={dateFormat(item.end_time)}
+              kind="medium16"
+              color="gray500"
+            />
+          </DetailInfoWrapper>
+          <DetailInfoWrapper>
+            <Font text="재구매 알림" kind="semi16" />
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <ToggleContainer
+                onPress={() => {
+                  // 재구매 알림 설정
+                  setIsOn(!isOn);
+                }}
+                isOn={isOn}>
+                <ToggleWheel
+                  style={[{ transform: [{ translateX: moveSwitchToggle }] }]}
+                />
+              </ToggleContainer>
+            </View>
+          </DetailInfoWrapper>
+          <DeleteButton onPress={onButtonPress}>
+            <Font text="삭제하기" kind="semi16" color="gray500" />
+          </DeleteButton>
+        </View>
       )}
     </Container>
   );
@@ -100,7 +168,11 @@ const calculateProgress = (startDateStr: string, endDateStr: string) => {
   return Math.min(100, Math.floor(progress));
 };
 
-const Container = styled.View`
+const dateFormat = (date: string) => {
+  return date.replaceAll('-', '.');
+};
+
+const Container = styled.TouchableOpacity`
   flex: 1;
   flex-direction: column;
   padding: 20px;
@@ -152,6 +224,45 @@ const UseLensDate = styled.View`
   flex-direction: row;
   flex: 1;
   justify-content: space-between;
+`;
+
+const DetailInfoWrapper = styled.View`
+  width: 100%;
+  padding: 22px 0px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  border-top: 1px;
+  border-top-color: ${color.gray200};
+  border-top-width: 1px;
+`;
+
+const ToggleContainer = styled.TouchableOpacity<{ isOn: boolean }>`
+  width: 46px;
+  height: 20px;
+  border-radius: 10px;
+  justify-content: center;
+  background-color: ${({ isOn }) => (isOn ? color.black : color.gray300)};
+  padding: 1px;
+`;
+
+const ToggleWheel = styled(Animated.View)`
+  width: 18px;
+  height: 18px;
+  background-color: ${color.white};
+  border-radius: 50%;
+`;
+
+const DeleteButton = styled.TouchableOpacity`
+  width: 100%;
+  padding: 14px 0px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${color.white};
+  border-radius: 10px;
+  border-color: ${color.gray300};
+  border-width: 1px;
+  margin-top: 10px;
 `;
 
 export default MyLensListItem;
