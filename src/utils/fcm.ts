9c040b6ff8a8912b5a2023783739177fd550a87f
hseun <hseun0732@dms.hs.kr>
2025-06-11 14:15:18 +0900

    add :: try-catch

--- src/utils/fcm.ts
+++ src/utils/fcm.ts
@@ -5,33 +5,44 @@ import messaging, {
 } from '@react-native-firebase/messaging';
 
 export const requestNotificationPermissionAndCreateChannel = async () => {
-  if (Platform.OS === 'android' && Platform.Version >= 33) {
-    await notifee.requestPermission();
-  }
-  await messaging().requestPermission();
+  try {
+    if (Platform.OS === 'android' && Platform.Version >= 33) {
+      await notifee.requestPermission();
+    }
+    await messaging().requestPermission();
 
-  await notifee.createChannel({
-    id: 'default',
-    name: '기본 채널',
-    importance: AndroidImportance.DEFAULT
-  });
+    await notifee.createChannel({
+      id: 'default',
+      name: '기본 채널',
+      importance: AndroidImportance.DEFAULT
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const pushAlarm = async (
   remoteMessage: FirebaseMessagingTypes.RemoteMessage
 ) => {
-  const { title, body } = remoteMessage.data as { title: string; body: string };
+  try {
+    const { title, body } = remoteMessage.data as {
+      title: string;
+      body: string;
+    };
 
-  if (!title && !body) {
-    return;
-  }
-
-  await notifee.displayNotification({
-    title: title,
-    body: body,
-    android: {
-      channelId: 'default',
-      smallIcon: 'ic_launched'
+    if (!title && !body) {
+      return;
     }
-  });
+
+    await notifee.displayNotification({
+      title: title,
+      body: body,
+      android: {
+        channelId: 'default',
+        smallIcon: 'ic_launcher'
+      }
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
